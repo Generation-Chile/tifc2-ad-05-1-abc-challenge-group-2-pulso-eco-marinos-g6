231172c29214b0c3f013eac09ed99b7f5e65ac6a
Trinidad Valenzuela <trinidad.valenzuela@genstudents.org>
2025-05-06 16:07:12 -0400

    tarea de css

---
+++ script.js
@@ -0,0 +1,12 @@
+/*Sección de JavaScript
+
+· Vincular script.js a HTML
+
+· Haz una función que aplique 3 colores aleatorios al azar.
+
+o Verde, Azul y Rojo.
+
+· Cuando alguien haga clic en cualquier etiqueta h5, haga que cambie a alguno de los 3 colores aleatorios.
+
+o Verde, Azul y Rojo.*/
+
